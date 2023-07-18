cda9b38f0305bb6285a91cd7548877b902617dde
sblekic <blekic.sandi@gmail.com>
2023-07-18 20:20:03 +0200

    postavljen moralis auth za testiranje na front

---
+++ environment.d.ts
@@ -0,0 +1,17 @@
+// iz : https://stackoverflow.com/questions/45194598/using-process-env-in-typescript
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: number;
+      APP_DOMAIN: string;
+      MORALIS_API_KEY: string;
+      APP_URI: string;
+      AUTH_SECRET: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+
 import storage from "./memory_storage";
-const siwe = require("siwe");
 
 const app = express(); // instanciranje aplikacije
-const port = 3000; // port na kojem će web server slušati
+const port = process.env.PORT || 3001; // port na kojem će web server slušati, čemu alternativa 3001?
 
-const domain = "localhost";
-const origin = "https://localhost/login";
-
-function createSiweMessage(address, statement) {
-  const siweMessage = new siwe.SiweMessage({
-    domain,
-    address,
-    statement,
-    uri: origin,
-    version: "1",
-    chainId: 1,
-  });
-  return siweMessage.prepareMessage();
-}
-
-console.log(
-  createSiweMessage(
-    "0x6Ee9894c677EFa1c56392e5E7533DE76004C8D94",
-    "This is a test statement."
-  )
-);
-
-app.use(cors()); // omogući CORS na svim rutama
+// omogući CORS na svim rutama
+// dopusti credentials poput kolačića u cross-origin zahtjevima
+app.use(cors({ credentials: true }));
 app.use(express.json()); // automatsko dekodiranje JSON poruku
+app.use(bodyParser.json()); // req.body je undefined bez ovoga
+app.use(cookieParser());
+
+const messageConfig = {
+  domain: process.env.APP_DOMAIN,
+  statement: "Please sign this message to confirm your identity.",
+  uri: process.env.APP_URI,
+  timeout: 60,
+};
 
 app.get("/", (req, res) => res.send("Hello World, ovaj puta preko browsera!"));
 
@@ -47,6 +43,84 @@ app.post("/posts", (req, res) => {
   res.json(data); // vrati podatke za referencu
 });
 
-app.listen(port, () =>
-  console.log("Slušam na: \x1b[36m%s\x1b[0m", `http://127.0.0.1:${port}`)
-);
+// request message to be signed by client
+app.post("/request-message", async (req, res) => {
+  console.log(req.body);
+  const { address, chain, network } = req.body;
+
+  try {
+    const message = await Moralis.Auth.requestMessage({
+      address,
+      chain,
+      ...messageConfig,
+    });
+
+    res.status(200).json(message);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.error(error);
+  }
+});
+
+app.post("/verify", async (req, res) => {
+  try {
+    const { message, signature } = req.body;
+
+    const { address, profileId } = (
+      await Moralis.Auth.verify({
+        message,
+        signature,
+        networkType: "evm",
+      })
+    ).raw;
+
+    const user = { address, profileId, signature };
+
+    // create JWT token
+    const token = jwt.sign(user, process.env.AUTH_SECRET);
+
+    // set JWT cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.error(error);
+  }
+});
+
+app.get("/authenticate", async (req, res) => {
+  const token = req.cookies.jwt;
+  if (!token) return res.sendStatus(403); // if the user did not send a jwt token, they are unauthorized
+
+  try {
+    const data = jwt.verify(token, process.env.AUTH_SECRET);
+    res.json(data);
+  } catch {
+    return res.sendStatus(403);
+  }
+});
+
+app.get("/logout", async (req, res) => {
+  try {
+    res.clearCookie("jwt");
+    return res.sendStatus(200);
+  } catch {
+    return res.sendStatus(403);
+  }
+});
+
+const startServer = async () => {
+  await Moralis.start({
+    apiKey: process.env.MORALIS_API_KEY,
+  });
+
+  app.listen(port, () =>
+    console.log("Slušam na: \x1b[36m%s\x1b[0m", `http://127.0.0.1:${port}`)
+  );
+};
+
+// Call startServer()
+startServer();
